151ce411cc2b54dafefdcb6a256d8e7b580f02d4
davidprager <david_prager@yahoo.com>
2021-12-04 20:05:41 -0500

    Initial commit

---
+++ HelloWorld.js
@@ -0,0 +1,14 @@
+/*
+ * File: HelloWorld.js
+ * -------------------
+ * This program displays "hello, world" on the console.  It is inspired
+ * by the first program in Brian Kernighan and Dennis Ritchie's classic
+ * book, The C Programming Language.
+ */
+
+"use strict";
+
+function HelloWorld() {
+   console.log("hello world");
+   console.log("I'm fred");
+}
